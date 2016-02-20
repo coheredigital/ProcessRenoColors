2351166a6483f1c88dda73be2a9960192639ca65
Adam Spruijt <adamspruijt@gmail.com>
2016-02-19 22:13:50 -0500

    init

---
+++ ProcessRenoColors.js
@@ -0,0 +1,5 @@
+$(function(){
+
+	$("#Inputfield_colors input").minicolors();
+
+});
---
+++ ProcessRenoColorsConfig.php
@@ -0,0 +1,61 @@
+<?php 
+
+
+class ProcessRenoColorsConfig extends ModuleConfig {
+    public function __construct() {
+
+        // add color picker js
+        $this->config->scripts->add("{$this->config->urls->ProcessRenoColors}minicolors/jquery.minicolors.min.js");        
+        $this->config->styles->add("{$this->config->urls->ProcessRenoColors}minicolors/jquery.minicolors.css");    
+
+        $this->config->scripts->add("{$this->config->urls->ProcessRenoColors}ProcessRenoColors.js");        
+
+        $this->add([
+            [
+
+                'label' => 'Main Colors',
+                'name' => 'colors',
+                'type' => 'fieldset',
+                "columnWidth" => 50,
+                "children" =>[
+                    [
+                        'name' => 'header_background_color',
+                        'label' => 'Header Background Color',
+                        'type' => 'text',
+                        'value' => "#4289cc",
+                        "columnWidth" => 25,
+                    ],
+                    [
+                        'name' => 'link_color',
+                        'label' => 'Link Color',
+                        'type' => 'text',
+                        'value' => "#0a89ff",
+                        "columnWidth" => 25,
+                    ],
+                ]
+            ],
+        ]);
+
+        // check if lessc is already loaded, fixes exception when less is loaded as default from composer / another source
+        // if (!class_exists("lessc")) {
+        //     require_once 'lessphp/lessc.inc.php';
+        // }
+
+        // $this->less = new lessc;
+        // $this->less->setFormatter("compressed");
+        
+        // $modules = wire('modules');
+
+        // $modules->addHookAfter('saveModuleConfigData', function($event){
+
+        //     $this->less->setVariables([
+        //         "header_background_color" => "{$this->header_background_color}",
+        //         "link_color" => "{$this->link_color}",
+        //     ]);
+
+        //     $this->less->compileFile("{$this->config->paths->ProcessRenoColors}RenoColors.less","{$this->config->paths->ProcessRenoColors}RenoColors.css");
+        // });  
+
+
+    }
+}
